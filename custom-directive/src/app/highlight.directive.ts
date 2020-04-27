994678e3036e8414cb6b9368f010d4efd5d0481e
rajesh002 <rajeshbtech16@gmail.com>
2020-04-27 16:23:44 +0530

    custom directive complete

---
+++ custom-directive/src/app/highlight.directive.ts
@@ -0,0 +1,31 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  Input,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(private el: ElementRef, private render: Renderer2) {}
+
+  @Input() defaultColor: string;
+
+  @Input('appHighlight') highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.changecolor(this.highlightColor || this.defaultColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.changecolor(null);
+  }
+
+  private changecolor(color: string) {
+    console.log(this.highlightColor);
+    this.render.setStyle(this.el.nativeElement, 'color', color);
+  }
+}
